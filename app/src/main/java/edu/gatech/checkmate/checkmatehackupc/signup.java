2b0494a91c03d4eb759337308eb9c816e4507a41
Kalya <ksubramanian33@gatech.edu>
2016-07-21 18:32:33 +0200

    settings

--- app/src/main/java/edu/gatech/checkmate/checkmatehackupc/signup.java
+++ app/src/main/java/edu/gatech/checkmate/checkmatehackupc/signup.java
@@ -1,7 +1,9 @@
 package edu.gatech.checkmate.checkmatehackupc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Signup extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+
+    public void onSignupPressed(View v) {
+        Intent intent = new Intent(this, Signup.class);
+        startActivity(intent);
     }
 }
