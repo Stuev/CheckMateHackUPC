ef3b316c70b8f22e2aaa3ec08438dd9504ba5b08
Stuev <swulber2015@gmail.com>
2016-07-22 08:02:54 +0200

    Cool UI

--- app/src/main/java/edu/gatech/checkmate/checkmatehackupc/Settings.java
+++ app/src/main/java/edu/gatech/checkmate/checkmatehackupc/Settings.java
@@ -3,8 +3,17 @@ package edu.gatech.checkmate.checkmatehackupc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Settings extends AppCompatActivity {
 
@@ -12,6 +21,32 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ListView lv = (ListView) findViewById(R.id.mateList);
+
+        List<String> your_array_list = new ArrayList<String>();
+
+        Database d = new Database(this);
+        System.out.println(Login.loggedIn);
+        for (User u : Login.loggedIn.getFriends().values()) {
+            String isOK = "";
+            try {
+                isOK =  ": " + d.isGood((Check) u);
+            } catch (Exception e) {
+                Log.d("HomeMate", "OOPS");
+            }
+            your_array_list.add(u.getUsername() + isOK);
+        }
+
+
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                your_array_list );
+
+        lv.setAdapter(arrayAdapter);
     }
 
     public void onChangeFrequencyPressed(View v) {
@@ -24,7 +59,10 @@ public class Settings extends AppCompatActivity {
     }
 
     public void onAddFriendPressed(View v) {
-
+        EditText friendName = (EditText) findViewById(R.id.newFriendName);
+        EditText username = (EditText) findViewById(R.id.newFriendUsername);
+        String friendNameString = (String) friendName.getText().toString();
+        String userNameString = (String) username.getText().toString();
     }
 
     public void onHomeButtonPressed(View v) {
@@ -32,5 +70,16 @@ public class Settings extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void onToggleButtonPressed(View v) {
+        ToggleButton a = (ToggleButton) findViewById(R.id.disableButton);
+        Boolean isEnabled = a.isEnabled();
+        if (isEnabled) {
+            //turn on alarm
+            Toast.makeText(Settings.this, "The check in notifications have been turned on.", Toast.LENGTH_SHORT).show();
+        } else {
+            //turn off alarm
+            Toast.makeText(Settings.this, "The check in notifications have been turned off.", Toast.LENGTH_SHORT).show();
+        }
+    }
 
 }
--- app/src/main/java/edu/gatech/checkmate/checkmatehackupc/SettingsMate.java
+++ app/src/main/java/edu/gatech/checkmate/checkmatehackupc/SettingsMate.java
@@ -3,7 +3,13 @@ package edu.gatech.checkmate.checkmatehackupc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SettingsMate extends AppCompatActivity {
 
@@ -11,6 +17,32 @@ public class SettingsMate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_mate);
+        ListView lv = (ListView) findViewById(R.id.checkList);
+
+        List<String> your_array_list = new ArrayList<String>();
+
+        Database d = new Database(this);
+        System.out.println(Login.loggedIn);
+        for (User u : Login.loggedIn.getFriends().values()) {
+            String isOK = "";
+            try {
+                isOK =  ": " + d.isGood((Check) u);
+            } catch (Exception e) {
+                Log.d("HomeMate", "OOPS");
+            }
+            your_array_list.add(u.getUsername() + isOK);
+        }
+
+
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                your_array_list );
+
+        lv.setAdapter(arrayAdapter);
     }
 
     public void onSettingsButtonPressed(View v) {
